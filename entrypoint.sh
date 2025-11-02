55bd5a4e06760a5de864f0f6b818cd28deac4e9f
Casper Bloemendaal <casperbloemendaal@gmail.com>
2025-11-02 14:01:21 +0100

    Fix command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd /github/workspace/
 npm install
 npm version $1 --git-tag-version false
 npx homey app version "$1" --changelog "$2"
-npx homey run build
+npx homey app build
 npm run check
 
 echo "version=$(cat app.json | jq --raw-output .version)" >> $GITHUB_OUTPUT
