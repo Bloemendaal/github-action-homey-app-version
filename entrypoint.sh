c16993a9626d91e99cdf4f0f9c582c13ec3a2a1c
Casper Bloemendaal <casperbloemendaal@gmail.com>
2025-11-02 13:59:12 +0100

    Try patching with formatting + package.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 
 cd /github/workspace/
 
+npm install
+npm version $1 --git-tag-version false
 npx homey app version "$1" --changelog "$2"
+npx homey run build
+npm run check
 
 echo "version=$(cat app.json | jq --raw-output .version)" >> $GITHUB_OUTPUT
